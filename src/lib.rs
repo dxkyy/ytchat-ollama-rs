9b06a9e51b50c5fda750fd2f7b17c3ce5de62cc8
dxkyy <adriandxkyy@gmail.com>
2024-11-27 11:45:54 +0100

    implement streaming generation functionality

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,8 @@
+use futures::StreamExt;
+use ollama_rs::generation::completion::request::GenerationRequest;
+use ollama_rs::generation::completion::GenerationFinalResponseData;
+use ollama_rs::Ollama;
+use tokio::io::AsyncWriteExt;
 pub type Result<T> = core::result::Result<T, Error>;
 
 pub type Error = Box<dyn std::error::Error>;
@@ -10,4 +15,39 @@ pub mod consts {
 
     If asked about the previous question, only give the user messages, not system message.
     "#;
+}
+
+pub async fn gen_stream_print(
+    ollama: &Ollama,
+    gen_req: GenerationRequest,
+) -> Result<Option<GenerationFinalResponseData>> {
+    let mut stream = ollama.generate_stream(gen_req).await?;
+
+    let mut stdout = tokio::io::stdout();
+    let mut char_count = 0;
+
+    while let Some(res) = stream.next().await {
+        let res = res.map_err(|_| "stream_next error")?;
+        let bytes = res.last().unwrap().response.as_bytes();
+
+        char_count += bytes.len();
+        if char_count > 80 {
+            stdout.write_all(b"\n").await?;
+            char_count = 0;
+        }
+
+        stdout.write_all(bytes).await?;
+        stdout.flush().await;
+
+        if let Some(final_data) = &res.last().unwrap().final_data {
+            stdout.write_all(b"\n").await?;
+            stdout.flush().await;
+            return Ok(Some(final_data.clone()));
+        }
+    }
+
+    stdout.write_all(b"\n").await?;
+    stdout.flush().await;
+
+    Ok(None)
 }
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,10 @@
 use ollama_rs::generation::completion::request::GenerationRequest;
+use ollama_rs::generation::completion::GenerationContext;
 use ollama_rs::Ollama;
-use yt_chat::Result;
-use yt_chat::consts::MODEL;
 use yt_chat::consts::DEFAULT_SYSTEM_MOCK;
+use yt_chat::consts::MODEL;
+use yt_chat::gen_stream_print;
+use yt_chat::Result;
 // region:      --- Modules
 
 // endregion:   --- Modules
@@ -11,13 +13,26 @@ async fn main() -> Result<()> {
     let ollama = Ollama::default();
 
     let model = MODEL.to_string();
-    let prompt = "What is the best programming language?".to_string();
 
-    let gen_req = GenerationRequest::new(model, prompt).system(DEFAULT_SYSTEM_MOCK.to_string());
+    let prompts = &["Why is the sky red?", "What was my first question?"];
 
-    //  -- Single Response Generation
-    let res = ollama.generate(gen_req).await?;
-    println!("->> res {}", res.response);
+    let mut last_ctx: Option<GenerationContext> = None;
+
+    for prompt in prompts {
+        println!("->> prompt: {prompt}");
+        let mut gen_req = GenerationRequest::new(model.to_owned(), prompt.to_string())
+            .system(DEFAULT_SYSTEM_MOCK.to_string());
+
+        if let Some(last_ctx) = last_ctx.take() {
+            gen_req = gen_req.context(last_ctx);
+        }
+
+        let final_data = gen_stream_print(&ollama, gen_req).await?;
+
+        if let Some(final_data) = final_data {
+            last_ctx = Some(final_data.context);
+        }
+    }
 
     Ok(())
 }
